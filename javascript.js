102a6c96f4685bc6ff0825986b82fca9e01736c1
ZaDivan <ivansadovnikov44@gmail.com>
2021-04-26 17:09:01 +0300

    pirkumu grozs

--- javascript.js
+++ javascript.js
@@ -1,4 +1,20 @@
-document.getElementById('Pievietot').addEventListener('click', () => {
-    Okno.style.display = 'block';
+function addItem(){
+    var ul = document.getElementById("list");
+    var candidate = document.getElementById("groza");
+    var candidate = document.getElementById("daudz");
+    var li = document.createElement("li");
+    li.setAttribute('id',groza.value);
+    li.appendChild(document.createTextNode(groza.value));
+    ul.appendChild(li);
+    li.setAttribute('id',daudz.value);
+    li.appendChild(document.createTextNode(daudz.value));
+    ul.appendChild(li);
+}
 
-})
+function removeItem(){
+    var ul = document.getElementById("list");
+    var groza = document.getElementById("groza");
+    var item = document.getElementById(groza.value);
+    var groza = document.getElementById("daudz");
+    var item = document.getElementById(daudz.value);
+    ul.removeChild(item);
