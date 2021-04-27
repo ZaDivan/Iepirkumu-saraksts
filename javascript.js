88e4d66cb7356a8614d861551b983ce5ba8b7291
ZaDivan <ivansadovnikov44@gmail.com>
2021-04-27 14:14:45 +0300

    pirkumu grozs

--- javascript.js
+++ javascript.js
@@ -1,20 +1,21 @@
 function addItem(){
-    var ul = document.getElementById("list");
-    var candidate = document.getElementById("groza");
-    var candidate = document.getElementById("daudz");
-    var li = document.createElement("li");
+    let ul = document.getElementById("list");
+    let candidate = document.getElementById("groza");
+    let candidate = document.getElementById("daudzums");
+    let li = document.createElement("li");
     li.setAttribute('id',groza.value);
     li.appendChild(document.createTextNode(groza.value));
     ul.appendChild(li);
-    li.setAttribute('id',daudz.value);
-    li.appendChild(document.createTextNode(daudz.value));
+    li.setAttribute('id',daudzums.value);
+    li.appendChild(document.createTextNode(daudzums.value));
     ul.appendChild(li);
 }
 
 function removeItem(){
-    var ul = document.getElementById("list");
-    var groza = document.getElementById("groza");
-    var item = document.getElementById(groza.value);
-    var groza = document.getElementById("daudz");
-    var item = document.getElementById(daudz.value);
+    let ul = document.getElementById("list");
+    let groza = document.getElementById("groza");
+    let item = document.getElementById(groza.value);
+    let groza = document.getElementById("daudzums");
+    let item = document.getElementById(daudzums.value);
     ul.removeChild(item);
+}
