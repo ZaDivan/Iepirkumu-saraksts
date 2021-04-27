74341b5d52d85dd75a991d586c9c80260194b5c6
ZaDivan <ivansadovnikov44@gmail.com>
2021-04-27 14:19:46 +0300

    pirkumu grozs

--- javascript.js
+++ javascript.js
@@ -1,21 +1,15 @@
 function addItem(){
     let ul = document.getElementById("list");
-    let candidate = document.getElementById("groza");
-    let candidate = document.getElementById("daudzums");
+    let candidate = document.getElementById("form");
     let li = document.createElement("li");
-    li.setAttribute('id',groza.value);
-    li.appendChild(document.createTextNode(groza.value));
-    ul.appendChild(li);
-    li.setAttribute('id',daudzums.value);
-    li.appendChild(document.createTextNode(daudzums.value));
+    li.setAttribute('id',form.value);
+    li.appendChild(document.createTextNode(form.value));
     ul.appendChild(li);
 }
 
 function removeItem(){
     let ul = document.getElementById("list");
-    let groza = document.getElementById("groza");
-    let item = document.getElementById(groza.value);
-    let daudzums = document.getElementById("daudzums");
-    let item = document.getElementById(daudzums.value);
+    let form = document.getElementById("form");
+    let item = document.getElementById(form.value);
     ul.removeChild(item);
 }
